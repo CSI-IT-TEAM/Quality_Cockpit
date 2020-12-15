ce46803d0b860d93e9186dae609f18d8bd324ff3
Ngọc Nguyễn Thanh <ngoc333@gmail.com>
2020-12-15 11:26:28 +0700

    add Form Dasboard

---
+++ 905.SMT_QUALITY_COCKPIT_DASBOARD/SMT_QUALITY_COCKPIT_DASBOARD.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace FORM
+{
+    partial class SMT_QUALITY_COCKPIT_DASBOARD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SMT_QUALITY_COCKPIT_DASBOARD));
+            this.pnHeader = new System.Windows.Forms.Panel();
+            this.cmdPm1 = new System.Windows.Forms.Button();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblHeader = new DevExpress.XtraEditors.LabelControl();
+            this.tmrTime = new System.Windows.Forms.Timer(this.components);
+            this.pnBody = new System.Windows.Forms.Panel();
+            this.pnHeader.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnHeader
+            // 
+            this.pnHeader.Controls.Add(this.cmdPm1);
+            this.pnHeader.Controls.Add(this.lblDate);
+            this.pnHeader.Controls.Add(this.lblHeader);
+            this.pnHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnHeader.Name = "pnHeader";
+            this.pnHeader.Size = new System.Drawing.Size(1924, 76);
+            this.pnHeader.TabIndex = 2;
+            // 
+            // cmdPm1
+            // 
+            this.cmdPm1.BackColor = System.Drawing.Color.Transparent;
+            this.cmdPm1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("cmdPm1.BackgroundImage")));
+            this.cmdPm1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.cmdPm1.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+            this.cmdPm1.FlatAppearance.BorderSize = 0;
+            this.cmdPm1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmdPm1.Font = new System.Drawing.Font("Calibri", 32.75F, System.Drawing.FontStyle.Bold);
+            this.cmdPm1.ForeColor = System.Drawing.Color.Navy;
+            this.cmdPm1.Location = new System.Drawing.Point(3, 3);
+            this.cmdPm1.Name = "cmdPm1";
+            this.cmdPm1.Size = new System.Drawing.Size(77, 70);
+            this.cmdPm1.TabIndex = 88;
+            this.cmdPm1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.cmdPm1.UseVisualStyleBackColor = false;
+            this.cmdPm1.Click += new System.EventHandler(this.cmdBack_Click);
+            // 
+            // lblDate
+            // 
+            this.lblDate.Dock = System.Windows.Forms.DockStyle.Right;
+            this.lblDate.Font = new System.Drawing.Font("Calibri", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.ForeColor = System.Drawing.Color.Black;
+            this.lblDate.Location = new System.Drawing.Point(1689, 0);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(235, 76);
+            this.lblDate.TabIndex = 5;
+            this.lblDate.Text = "2020-07-22\r\n10:00:00";
+            this.lblDate.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.Appearance.Font = new System.Drawing.Font("Calibri", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.Appearance.Options.UseFont = true;
+            this.lblHeader.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
+            this.lblHeader.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lblHeader.LineVisible = true;
+            this.lblHeader.Location = new System.Drawing.Point(0, 0);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(1622, 76);
+            this.lblHeader.TabIndex = 3;
+            this.lblHeader.Text = "        Quality Dashboard: VJ Monthly Performance";
+            // 
+            // tmrTime
+            // 
+            this.tmrTime.Enabled = true;
+            this.tmrTime.Interval = 1000;
+            this.tmrTime.Tick += new System.EventHandler(this.tmrTime_Tick);
+            // 
+            // pnBody
+            // 
+            this.pnBody.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnBody.Location = new System.Drawing.Point(0, 76);
+            this.pnBody.Name = "pnBody";
+            this.pnBody.Size = new System.Drawing.Size(1924, 986);
+            this.pnBody.TabIndex = 3;
+            // 
+            // SMT_QUALITY_COCKPIT_DASBOARD
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1924, 1062);
+            this.Controls.Add(this.pnBody);
+            this.Controls.Add(this.pnHeader);
+            this.Name = "SMT_QUALITY_COCKPIT_DASBOARD";
+            this.Text = "SMT_SCADA_COCKPIT_FORM2";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.VisibleChanged += new System.EventHandler(this.Form_VisibleChanged);
+            this.pnHeader.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel pnHeader;
+        public System.Windows.Forms.Button cmdPm1;
+        private System.Windows.Forms.Label lblDate;
+        private DevExpress.XtraEditors.LabelControl lblHeader;
+        private System.Windows.Forms.Timer tmrTime;
+        private System.Windows.Forms.Panel pnBody;
+    }
+}
---
+++ 905.SMT_QUALITY_COCKPIT_DASBOARD/SMT_QUALITY_COCKPIT_DASBOARD.cs
@@ -0,0 +1,162 @@
+﻿using DevExpress.Utils;
+using DevExpress.XtraCharts;
+using DevExpress.XtraGrid.Views.BandedGrid;
+using System;
+using System.Data;
+using System.Data.OracleClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FORM
+{
+    public partial class SMT_QUALITY_COCKPIT_DASBOARD : Form
+    {
+        public SMT_QUALITY_COCKPIT_DASBOARD()
+        {
+            InitializeComponent();
+        }
+        int _time = 0;
+        string _CurrentDay = DateTime.Now.ToString("MMM - dd");
+
+        #region Event
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form_VisibleChanged(object sender, EventArgs e)
+        {
+            if (Visible)
+            {
+                tmrTime.Start();
+            }
+            else
+            {
+                tmrTime.Stop();
+            }
+
+        }
+
+        private void tmrTime_Tick(object sender, EventArgs e)
+        {
+            lblDate.Text = string.Format(DateTime.Now.ToString("yyyy-MM-dd\nHH:mm:ss"));
+            _time++;
+            if (_time >= 30)
+            {
+                _time = 0;
+            }
+        }
+
+        private void cmdBack_Click(object sender, EventArgs e)
+        {
+            ComVar.Var.callForm = "back";
+        }
+
+        #endregion Event
+
+        #region Database
+        private DataTable Fn_SelectDataCombo(string argType, string argPlant, string argLine )
+        {           
+            COM.OraDB MyOraDB = new COM.OraDB();
+            DataSet ds_ret;
+            try
+            {
+                string process_name = "SEPHIROTH.PKG_SMT_QUALITY_COCKPIT_02.NPI_COMBO_SELECT";
+
+                MyOraDB.ReDim_Parameter(4);
+                MyOraDB.Process_Name = process_name;
+
+                MyOraDB.Parameter_Name[0] = "V_P_TYPE";
+                MyOraDB.Parameter_Name[1] = "V_P_PLANT";
+                MyOraDB.Parameter_Name[2] = "V_P_LINE";
+                MyOraDB.Parameter_Name[3] = "OUT_CURSOR";
+
+                MyOraDB.Parameter_Type[0] = (int)OracleType.VarChar;
+                MyOraDB.Parameter_Type[1] = (int)OracleType.VarChar;
+                MyOraDB.Parameter_Type[2] = (int)OracleType.VarChar;
+                MyOraDB.Parameter_Type[3] = (int)OracleType.Cursor;
+
+                MyOraDB.Parameter_Values[0] = argType;
+                MyOraDB.Parameter_Values[1] = argPlant;
+                MyOraDB.Parameter_Values[2] = argLine;
+                MyOraDB.Parameter_Values[3] = "";
+
+                MyOraDB.Add_Select_Parameter(true);
+                ds_ret = MyOraDB.Exe_Select_Procedure();
+
+                if (ds_ret == null) return null;
+                return ds_ret.Tables[process_name];
+            }
+            catch
+            {
+                return null;
+            }
+            
+        }
+
+        public async Task<DataTable> Fn_SelectDataGrid(string argType, string argPlan, string argLine, string argFromDate, string argToDate)
+        {
+            return await Task.Run(() => {
+                COM.OraDB MyOraDB = new COM.OraDB();
+                DataSet ds_ret;
+                MyOraDB.ShowErr = true;
+                try
+                {
+                    string process_name = "SEPHIROTH.PKG_SMT_QUALITY_COCKPIT_02.NPI_DATA_SELECT";
+
+                    MyOraDB.ReDim_Parameter(7);
+                    MyOraDB.Process_Name = process_name;
+
+                    MyOraDB.Parameter_Name[0] = "V_P_TYPE";
+                    MyOraDB.Parameter_Name[1] = "V_P_FACTORY";
+                    MyOraDB.Parameter_Name[2] = "V_P_PLANT";
+                    MyOraDB.Parameter_Name[3] = "V_P_MLINE";
+                    MyOraDB.Parameter_Name[4] = "V_P_FROM";
+                    MyOraDB.Parameter_Name[5] = "V_P_TO";
+                    MyOraDB.Parameter_Name[6] = "OUT_CURSOR";
+
+                    MyOraDB.Parameter_Type[0] = (int)OracleType.VarChar;
+                    MyOraDB.Parameter_Type[1] = (int)OracleType.VarChar;
+                    MyOraDB.Parameter_Type[2] = (int)OracleType.VarChar;
+                    MyOraDB.Parameter_Type[3] = (int)OracleType.VarChar;
+                    MyOraDB.Parameter_Type[4] = (int)OracleType.VarChar;
+                    MyOraDB.Parameter_Type[5] = (int)OracleType.VarChar;
+                    MyOraDB.Parameter_Type[6] = (int)OracleType.Cursor;
+
+                    MyOraDB.Parameter_Values[0] = argType;
+                    MyOraDB.Parameter_Values[1] = "2110";
+                    MyOraDB.Parameter_Values[2] = argPlan;
+                    MyOraDB.Parameter_Values[3] = argLine;
+                    MyOraDB.Parameter_Values[4] = argFromDate;
+                    MyOraDB.Parameter_Values[5] = argToDate;
+                    MyOraDB.Parameter_Values[6] = "";
+
+                    MyOraDB.Add_Select_Parameter(true);
+                    ds_ret = MyOraDB.Exe_Select_Procedure();
+
+                    if (ds_ret == null) return null;
+                    return ds_ret.Tables[process_name];
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex);
+                    return null;
+                }
+            });
+        }
+
+        #endregion DB
+
+
+       
+
+
+        
+
+        
+    }
+}
